c2c9eb522d8e8f93c8f0d08e29c76419873e2f95
InnaTovstik <kitimail69@gmail.com>
2021-09-27 12:00:44 +0300

    Rewrite code

--- src/test/java/com/company/StringLinkedListImplTest.java
+++ src/test/java/com/company/StringLinkedListImplTest.java
@@ -7,96 +7,89 @@ import org.junit.jupiter.api.Test;
 public class StringLinkedListImplTest {
 
     private static StringLinkedListImpl subject;
+    private String str1 = "aaa";
+    private String str2 = "bbb";
+    private String str3 = "ccc";
+    private int size = 3;
+    private static String strToFirst = "ddd";
+    private static String strToLast = "zzz";
+    private static String strToIndex = "nnn";
+
 
     @BeforeEach
      public void setSubject(){
         subject = new StringLinkedListImpl();
-        String str1 = "aaa";
-        String str2 = "bbb";
-        String str3 = "ccc";
         //заполняем данными нашего метода
-        subject.add(str1,0);
+        subject.add(str1, 0);
         subject.add(str2, 1);
         subject.add(str3, 2);
-    }
+     }
 
     @Test
     public void shouldAddFirstToEmptyList(){
-        String str = "ddd";
         // создаем пустой список
         subject = new StringLinkedListImpl();
-        subject.addFirst(str);
-        Assertions.assertEquals("ddd", subject.getFirst());
+        subject.addFirst(strToFirst);
+        Assertions.assertEquals(strToFirst, subject.getFirst());
     }
 
     @Test
     public void shouldAddFirstToNotEmptyList(){
-        String str = "ddd";
-        subject.addFirst(str);
-        Assertions.assertEquals("ddd", subject.getFirst());
+        subject.addFirst(strToFirst);
+        Assertions.assertEquals(strToFirst, subject.getFirst());
     }
 
     @Test
     public void shouldAddToFirst(){
-        String str = "ddd";
-        subject.add(str, 0);
-        Assertions.assertEquals("ddd", subject.getFirst());
+        subject.add(strToFirst, 0);
+        Assertions.assertEquals(strToFirst, subject.getFirst());
     }
 
     @Test
     public void shouldAddToLast(){
-        String str = "ddd";
-        subject.addLast(str);
-        Assertions.assertEquals("ddd", subject.getLast());
+        subject.addLast(strToLast);
+        Assertions.assertEquals(strToLast, subject.getLast());
     }
 
     @Test
     public void shouldAddForIndex(){
-        String str = "yyy";
-        subject.add(str, 2);
-        Assertions.assertEquals("yyy", subject.get(2));
+        subject.add(strToIndex, 2);
+        Assertions.assertEquals(strToIndex, subject.get(2));
     }
 
-    @Test
-    public void shouldAddLast(){
-        String str = "zzz";
-        subject.addLast(str);
-        Assertions.assertEquals("zzz", subject.getLast());
-    }
-
-    @Test
+   @Test
     public void shouldGetFirst(){
         String actual = subject.getFirst();
-        Assertions.assertEquals("aaa", actual);
+        Assertions.assertEquals(str1, actual);
     }
 
     @Test
     public void shouldGetLast(){
         String actual = subject.getLast();
-        Assertions.assertEquals("ccc", actual);
+        Assertions.assertEquals(str3, actual);
     }
 
     @Test
     public void shouldRemoveFirst(){
         String actual = subject.removeFirst();
-        Assertions.assertEquals("aaa", actual);
+        Assertions.assertEquals(str1, actual);
     }
     @Test
     public void shouldRemoveOnIndex(){
         String actual = subject.remove(1);
-        Assertions.assertEquals("bbb", actual);
+        Assertions.assertEquals(str2, actual);
     }
 
     @Test
     public void shouldRemoveLast(){
         String actual = subject.removeLast();
-        Assertions.assertEquals("ccc", actual);
+        Assertions.assertEquals(str3, actual);
     }
 
     @Test
     public void shouldSize(){
         int actual = subject.size();
-        Assertions.assertEquals(3, actual);
+        Assertions.assertEquals(size, actual);
     }
 
 }
